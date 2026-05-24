98d60347532023c0f1ed21f8af50c67f09fd9e39
DangKimAn <dangkiman41005@gmail.com>
2026-05-25 01:06:33 +0700

    fix : conflict

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,15 +3,26 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
 import { GoogleStrategy } from './strategies/google.strategy';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    PrismaModule, 
-    JwtModule.register({}), // Hoặc dùng ConfigService để lấy secret key từ file .env
+    PrismaModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }), //Cấu hình Passport nhận mặc định jwt
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_ACCESS_SECRET'),
+        signOptions: { expiresIn: '15m' },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy],
-  exports: [AuthService],
+  providers: [AuthService, JwtStrategy, JwtRefreshStrategy, GoogleStrategy], //BẮT BUỘC: Điền cả 2 vào đây để NestJS không bắt lỗi thiếu Dependency khi compile
+  exports: [AuthService, PassportModule, JwtModule], //Export ra ngoài cho các Module khác xài chung Guard
 })
-export class AuthModule {}
+export class AuthModule { }
